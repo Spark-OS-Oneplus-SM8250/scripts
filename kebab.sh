6a98cf40fd0170db0a1010855ce4c0575dcefe11
DawfukFR <dawfukfr@gmail.com>
2023-06-16 23:12:50 +0200

    scripts: Adding setup scripts for build `SparkOS` on Oneplus SM8250
    Signed-off-by: DawfukFR <dawfukfr@gmail.com>

---
+++ kebab.sh
@@ -0,0 +1,31 @@
+cd ..
+echo 'Starting to clone stuffs needed for your device'
+echo 'Cloning kebab device and vendor trees [1/6]'
+
+rm -rf device/oneplus/kebab && rm -rf vendor/oneplus/kebab
+git clone https://github.com/Spark-OS-Oneplus-SM8250/device_oneplus_kebab device/oneplus/kebab
+git clone https://github.com/Spark-OS-Oneplus-SM8250/vendor_oneplus_kebab vendor/oneplus/kebab
+
+echo 'Cloning common device and vendor trees [2/6]'
+rm -rf device/oneplus/sm8250-common && rm -rf vendor/oneplus/sm8250-common
+git clone https://github.com/Spark-OS-Oneplus-SM8250/device_oneplus_sm8250-common device/oneplus/sm8250-common
+git clone https://github.com/Spark-OS-Oneplus-SM8250/vendor_oneplus_sm8250-common vendor/oneplus/sm8250-common
+
+echo 'Cloning kernel tree [3/6]'
+rm -rf kernel/oneplus/sm8250
+git clone https://github.com/Spark-OS-Oneplus-SM8250/kernel_oneplus_sm8250 kernel/oneplus/sm8250
+
+echo 'Cloning hardware oplus [4/6]'
+rm -rf hardware/oplus
+git clone https://github.com/Spark-OS-Oneplus-SM8250/hardware_oplus hardware/oplus
+
+echo 'Cloning additionnal stuff [5/6]'
+rm -rf vendor/oneplus/camera
+git clone https://gitlab.com/cjh1249131356/vendor_oneplus_camera vendor/oneplus/camera
+
+echo 'Recloning somes source stuff but readapted for your device [6/6]'
+rm -rf frameworks/av && rm -rf frameworks/base rm -rf device/spark/sepolicy && rm -rf vendor/spark
+git clone https://github.com/Spark-OS-Oneplus-SM8250/frameworks_av frameworks/av
+git clone https://github.com/Spark-OS-Oneplus-SM8250/frameworks_base frameworks/base
+git clone https://github.com/Spark-OS-Oneplus-SM8250/device_spark_sepolicy device/spark/sepolicy
+git clone https://github.com/Spark-OS-Oneplus-SM8250/platform_vendor_spark vendor/spark
